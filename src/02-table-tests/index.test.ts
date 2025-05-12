d553deda36441e4a2faf4a9d4bb6af7a39f79616
Maksim Buga <maksim.booga@gmail.com>
2025-05-12 19:34:34 +0400

    feat: add 02-table-tests solution

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,33 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+ import {  simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 5, b: 3, action: Action.Subtract, expected: 2 },
+  { a: 4, b: 2, action: Action.Multiply, expected: 8 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 2, b: 0, action: Action.Divide, expected: Infinity },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)(
+    'should correctly calculate a action b',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toEqual(expected);
+    }
+  );
+
+  test('should return null for an invalid action', () => {
+    expect(simpleCalculator({ a: 2, b: 3, action: '%' })).toBeNull();
   });
-  // Consider to use Jest table tests API to test all cases above
+
+  test('should return null for invalid arguments', () => {
+    expect(simpleCalculator({ a: '2', b: 3, action: Action.Add })).toBeNull();
+    expect(simpleCalculator({ a: 2, b: '3', action: Action.Add })).toBeNull();
+    expect(simpleCalculator({ a: null, b: 3, action: Action.Add })).toBeNull();
+    expect(simpleCalculator({ a: 2, b: undefined, action: Action.Add })).toBeNull();
+  });
 });
